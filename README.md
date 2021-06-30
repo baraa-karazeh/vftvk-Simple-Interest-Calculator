5107a1ed01dafce9fa8e79627d0c65738e2df733
baraa-karazeh <eng.baraa.karazeh@gmail.com>
2021-06-30 15:12:09 +0300

    added comments to index, style, script and README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # coding-project-template
+
+This app created by Muhammad Baraa Karazeh for a non-governmental charity organization called Everyone Can Get Rich. This app titled Simple Interest Calculator, will compute the interest one can earn given the amount you wish to save, the number of years you can keep it with the bank and the interest rate the bank would pay you.
+
+PS: this app is only for educational purpose.
--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 
- 
-    function compute()
+ /* this functin activates once the button compute interest clicked*/
+ function compute()
     {
-        let principal = document.getElementById("principal").value;
-        let rate = document.getElementById("rate").value;
-        let years = document.getElementById("years").value;
-        let year=new Date().getFullYear()+parseInt(years);
-        let interest=principal*years*(rate/100);
-        
-        if( principal<=0){
-            alert("Please enter a posiive number in the amount area to compute the interest");
-            document.querySelector("input").focus();
-            
-        }
-        else{
-            return output();
-        }
 
-        function output(){
+    /* initializing the input values entered by a user*/
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
 
-            let showPrincipal="\<br\>If you deposit "+`<mark>${principal}</mark>`;
-            let showRate=",\<br\>at an interest rate of "+`<mark>${rate}</mark>`;
-            let showInterest="%.\<br\>You will receive an amount of "+`<mark>${interest.toFixed(0)}</mark>`;
-            let showYear=",\<br\>in the year "+`<mark>${year}</mark>`+"\<br\>";
+    /* converting the number of years being chosen by a user to the actual year*/
+    let year=new Date().getFullYear()+parseInt(years);
 
-            let output=document.getElementById("result").innerHTML=showPrincipal+showRate+showInterest+showYear;
+    /* the formula that calculates the interst amount*/
+    let interest=principal*years*(rate/100);
+    
+    /* validates the input values entered by a user to output the result*/
+    if( principal<=0){
+        alert("Please enter a posiive number in the amount area to compute the interest");
+        document.querySelector("input").focus();
+    }
+    else{
+        return output();
+    }
+
+    /* this function shows the computed result*/
+    function output()
+    {
+        let showPrincipal="\<br\>If you deposit "+`<mark>${principal}</mark>`;
+        let showRate=",\<br\>at an interest rate of "+`<mark>${rate}</mark>`;
+        let showInterest="%.\<br\>You will receive an amount of "+`<mark>${interest.toFixed(0)}</mark>`;
+        let showYear=",\<br\>in the year "+`<mark>${year}</mark>`+"\<br\>";
+
+        let output=document.getElementById("result").innerHTML=showPrincipal+showRate+showInterest+showYear;
         }
     }
 
-    function updateRate() 
+function updateRate() 
     {
-        let ratevalue = document.getElementById("rate").value;
-        document.getElementById("rate_val").innerText=ratevalue+"\%";
+    let ratevalue = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=ratevalue+"\%";
     }
   
     
